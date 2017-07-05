4ebd551bbdffafbbd8854e64fa546fafd014cfc1
ArtemNovorai <artnovorai@gmail.com>
2017-07-05 22:05:21 +0300

    Methods and properties of array

---
+++ script.js
@@ -0,0 +1,7 @@
+var w = [1,'hello',67,'werber',5,345];
+ for(var index in w) {
+document.write('Ячейка массива - ' + index + ' - значение данной ячейки - ' + w[index] + '<br>' );
+   
+   var arr = [1,2,3,4,['Artem','Novorai']];
+   document.write(arr[4][1] + '<br>');
+ }
